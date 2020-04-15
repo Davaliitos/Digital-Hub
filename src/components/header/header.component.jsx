e0d26cf0ce36bea7bd54db7c34a43d83e955d887
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-04-15 04:59:20 -0500

    Fix header style when hover and when route is selected

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,54 +1,50 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import {selectCurrentUser} from '../../redux/user/user.selectors';
-import {setCurrentUser} from '../../redux/user/user.actions';
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { setCurrentUser } from "../../redux/user/user.actions";
 
-import './header.style.scss';
+import "./header.style.scss";
 
-const Header = ({currentUser, setCurrentUser}) => (
+const Header = ({ currentUser, setCurrentUser }) => {
+
+  let location = useLocation();
+  let pathName = location.pathname;
+
+  return (
     <div className="header">
-        <div className="options">
-            <Link className="option" to="/">
-                COMPANY
-            </Link>
-            {
-                currentUser ? (
-                    <Link className="option" to="/">
-                        HOME
-                    </Link>
-                )
-                : null
-            }
-            {
-                currentUser ? (
-                    <Link className="option" to="/transfer">
-                        TRANSFER
-                    </Link>
-                )
-                : null
-            }            
-        </div>
-        {
-            currentUser ? (
-                <Link className="logout" to="/" onClick={() => setCurrentUser(null)}>
-                    LOG OUT
-                </Link>
-            )
-            : null
-        }
-        
+      <div className="options">
+        <Link className="option" to="/">
+          COMPANY
+        </Link>
+        {currentUser ? (
+          <Link className={`${pathName === '/' ? 'enabled' : ''} option`} to="/">
+            HOME
+          </Link>
+        ) : null}
+        {currentUser ? (
+          <Link className={`${pathName === '/transfer' ? 'enabled' : ''} option`} to="/transfer">
+            TRANSFER
+          </Link>
+        ) : null}
+      </div>
+      {currentUser ? (
+        <Link className="logout" to="/" onClick={() => setCurrentUser(null)}>
+          LOG OUT
+        </Link>
+      ) : null}
     </div>
-)
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-    currentUser : selectCurrentUser
-})
+  currentUser: selectCurrentUser,
+});
 
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser : user => dispatch(setCurrentUser(user))
-  })
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default connect(mapStateToProps,mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
